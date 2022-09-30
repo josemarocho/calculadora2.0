bc9cc5037d5e4bc9c3c271bedc98f6fbdcabef8d
josemarocho <josemarocho123@gmail.com>
2022-09-29 19:39:49 -0500

    mi primer commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Smit
+ */
+public class Calculadora {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pantalla calc=new Pantalla();
+        calc.show();
+    }
+    
+}
